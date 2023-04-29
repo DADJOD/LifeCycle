a9a5fb2fa8bb5ad30ff6a85465251979b559d59d
DADJOD <kamu57@mail.ru>
2023-04-29 19:14:14 +0300

    commit_4

--- app/src/main/java/com/example/lifecycle/EditActivity.kt
+++ app/src/main/java/com/example/lifecycle/EditActivity.kt
@@ -1,11 +1,14 @@
 package com.example.lifecycle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 
-class EditActivity : AppCompatActivity() {
+class EditActivity : AppCompatActivity(), View.OnClickListener {
     lateinit var edit: EditText
     lateinit var button: Button
 
@@ -15,5 +18,60 @@ class EditActivity : AppCompatActivity() {
 
         edit = findViewById(R.id.edit)
         button = findViewById(R.id.button)
+
+        val intent = intent
+//        val strIntent = intent.getStringExtra("NAME")
+        edit.setText(intent.getStringExtra("NAME"))
+
+        button.setOnClickListener(this)
+
+        Log.d("happySDK", Thread.currentThread().stackTrace[2].methodName)
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d("happySDK", Thread.currentThread().stackTrace[2].methodName)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d("happySDK", Thread.currentThread().stackTrace[2].methodName)
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.d("happySDK", Thread.currentThread().stackTrace[2].methodName)
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d("happySDK", Thread.currentThread().stackTrace[2].methodName)
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        Log.d("happySDK", Thread.currentThread().stackTrace[2].methodName)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d("happySDK", Thread.currentThread().stackTrace[2].methodName)
+    }
+
+    override fun onClick(v: View?) {
+        val inputText = edit.text.toString()
+
+        val intent2 = Intent()
+        intent2.putExtra("INPUT_TEXT", inputText)
+        setResult(RESULT_OK, intent2)
+        onBackPressed()
+    }
+
+
 }
--- app/src/main/java/com/example/lifecycle/MainActivity.kt
+++ app/src/main/java/com/example/lifecycle/MainActivity.kt
@@ -7,7 +7,9 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     lateinit var text: TextView
 
@@ -20,10 +22,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         text = findViewById(R.id.textView)
         text.setOnClickListener(this)
-    }
-
-    override fun onClick(v: View?) {
-        startActivity(Intent(this, EditActivity::class.java))
     }
 
     override fun onStart() {
@@ -60,5 +58,27 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onDestroy()
 
         Log.d("happySDK", Thread.currentThread().stackTrace[2].methodName)
+    }
+
+    override fun onClick(v: View?) {
+        val intent = Intent(this, EditActivity::class.java)
+        intent.putExtra("NAME", "JOHN")
+
+//        startActivity(intent)
+        startActivityForResult(intent, 444)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 444) {
+//            val name = data?.getStringExtra("INPUT_TEXT")
+//            if (name != null) {
+//                Toast.makeText(this, "Name: $name", Toast.LENGTH_SHORT).show()
+//                text.text = name
+//            }
+            text.text = data?.getStringExtra("INPUT_TEXT")
+            return
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
